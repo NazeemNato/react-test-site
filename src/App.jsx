e9b187e81310c1b766755b8c3e7a66b17bdc418c
nazeem <nazeemnob18@gmail.com>
2022-01-21 14:06:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App(){
+  let name = "Nazeem";
+  let description = "This is my first react app";
+  
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+export default App
